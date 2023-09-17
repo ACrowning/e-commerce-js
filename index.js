0252028fe76accaffccc2cafeabc3c38167cdf42
ACrowning <holovan24@gmail.com>
2023-09-17 12:04:22 +0300

    ShoppingCart upgrades, cod cleanered

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const closeCartButton = document.getElementById("closeCart");
 const containerOverlayCount = document.querySelector(".count");
 const cartInContainer = document.querySelector(".cartInContainer");
 
-const creation = (product, container, items, productsOfCart) => {
+const creation = (product, container, items, storedOfProducts) => {
   const newLi = document.createElement("li");
   newLi.className = "product";
   const overlay = document.createElement("div");
@@ -97,7 +97,7 @@ const creation = (product, container, items, productsOfCart) => {
       cartCount,
       parseCartCount,
       parseNewCount,
-      productsOfCart
+      storedOfProducts
     );
     if (product.count === 0) {
       addCart.disabled = true;
@@ -162,11 +162,8 @@ const btnAddCart = (
   theCartCount,
   cartCountParsed,
   newCountParsed,
-  productsOfCart
+  storedOfProducts
 ) => {
-  // const eachCart = document.createElement("div");
-  // cartInContainer.appendChild(eachCart);
-
   if (theNewCount.value <= 0) {
     alert(ALERT_COUNT_TEXT);
   } else if (theNewCount.value > product.count) {
@@ -174,23 +171,20 @@ const btnAddCart = (
   } else {
     theCartCount.textContent = cartCountParsed + newCountParsed;
     product.count = product.count - theNewCount.value;
-    const cartProduct = `${product.title}, ${product.description} ${newCountParsed}`;
-    // productsOfCart.push(cartProduct);
-    // eachCart.textContent = `${product.title}, ${product.description} ${newCountParsed}`;
-    localStorage.setItem("cartStorage", JSON.stringify(productsOfCart));
+    storedOfProducts.push(product);
   }
   theNewCount.value = 1;
 };
 
-const createUl = (items, container) => {
+const createUl = (items, container, storedOfProducts) => {
   items.forEach((product) => {
-    creation(product, container, items);
+    creation(product, container, items, storedOfProducts);
   });
 };
 
-const add = (product, container, items, productsOfCart) => {
+const add = (product, container, items) => {
   items.push(product);
-  creation(product, container, items, productsOfCart);
+  creation(product, container, items);
 };
 
 const onAdd = (
@@ -198,8 +192,7 @@ const onAdd = (
   titleOnAdd,
   descriptionOnAdd,
   container,
-  toInputCount,
-  productsOfCart
+  toInputCount
 ) => {
   if (titleOnAdd.value.length === 0) {
     return alert(ALERT_TEXT);
@@ -218,7 +211,7 @@ const onAdd = (
     count: Number(toInputCount.value),
   };
 
-  add(newProduct, container, items, productsOfCart);
+  add(newProduct, container, items);
 
   titleOnAdd.value = INPUT_VOID_VALUE;
   descriptionOnAdd.value = INPUT_VOID_VALUE;
@@ -231,44 +224,22 @@ const enterBtn = (event) => {
   }
 };
 
-// const openCart = () => {
-//   shoppingCart.classList.toggle("visible");
-// };
-
-// const closeCart = (event) => {
-//   if (event.key === "Escape") {
-//     shoppingCart.classList.toggle("visible").remove();
-//   }
-// };
-
 const initCart = () => {
   cartCount.textContent = storedProducts.length;
 };
 
 const init = () => {
   initCart();
-
-  // const storedProducts1 = JSON.parse(localStorage.getItem("cartStorage"));
-  // const shoppingCartProducts = storedProducts1 ?? [];
-  // shoppingCartButton.addEventListener("click", () => {
-  //   console.log(shoppingCartProducts);
-  // });
+  shoppingCartButton.addEventListener("click", () => {
+    console.log(storedProducts);
+  });
 
   button.addEventListener("click", () => {
-    onAdd(
-      products,
-      inputTitle,
-      inputDescription,
-      list,
-      inputCount,
-      shoppingCartProducts
-    );
+    onAdd(products, inputTitle, inputDescription, list, inputCount);
   });
 
   document.addEventListener("keydown", enterBtn);
-  createUl(products, list);
-  // closeCartButton.addEventListener("click", openCart);
-  // document.addEventListener("keydown", closeCart);
+  createUl(products, list, storedProducts);
 };
 
 init();
